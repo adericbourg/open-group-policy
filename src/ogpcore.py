a481448069336703033adc103726c4213de35a14
Alban Dericbourg <a.dericbourg+github@gmail.com>
2009-02-07 23:26:40 +0000

    OGPCore class (draft) structure

---
+++ src/ogpcore.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*
+
+class ogpcore:
+	__init__(self, host, port, dn=none, passwd=none, cert=none):
+		pass
+
+	createOU(dn, others=none):
+		pass
+
+	createMachine(dn, others=none):
+		pass
+
+	push(file, ou, overwrite):
+		pass
+
+	pull(ou, recursive):
+		pass
+
+	pullFile(ou, file, recursive):
+		pass
+
+	getFileList(ou, recursive):
+		pass
+
+	build(ou, recursive):
+		pass
+
+	buildFile(ou, recusrive):
+		pass
