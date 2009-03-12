ca91a5dd1bf444ee625451dc0ebf6b6c18089de7
Alban Dericbourg <a.dericbourg+github@gmail.com>
2009-03-12 12:59:06 +0000

    Drafts

---
+++ src/OgpXmlElement.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*
+
+import xml.dom as dom
+
+class OgpXmlElement(object):
+
+	def __init__(self, xml):
+		pass
+
+	def getAttrs(self):
+		return
+
+	def getAttr(self, name):
+		return
+
+	def isBlocking(self):
+		return
+
+	def setAttr(self):
+		pass
+
+	def setText(self):
+		pass
+
+	def addChild(self):
+		pass
+
+	def rmChild(self):
+		pass
+
+
+
