e63ff4feb080a8211b8676d766dde002df9083ea
Alban Dericbourg <a.dericbourg+github@gmail.com>
2009-03-18 12:06:58 +0000

    Let's clean some stuff!

--- src/DomElementMethods/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*
-
-from lxml.etree import *
-from ElementMethods import *
-
-OGP_PARSER = XMLParser()
-OGP_PARSER.set_element_class_lookup(ElementDefaultClassLookup(element=OgpElement))
-Element = OGP_PARSER.makeelement
